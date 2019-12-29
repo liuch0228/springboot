8a207099d609e49c2e846cb64ea9435c98f041a8
liuch0228 <1402828263@qq.com>
2019-12-29 12:21:21 +0800

    spring initializr初始化项目

---
+++ springboot-demo2/src/main/java/com/example/lchtest/springbootdemo2/SpringbootDemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.lchtest.springbootdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemo2Application.class, args);
+	}
+
+}
---
+++ springboot-demo2/src/test/java/com/example/lchtest/springbootdemo2/SpringbootDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lchtest.springbootdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
